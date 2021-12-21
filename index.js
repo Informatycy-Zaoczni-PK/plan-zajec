32952095029ae1afceee70f312630a2040c3f8cf
Mateusz Borowiecki <borowieckimateusz1@gmail.com>
2021-12-21 22:21:35 +0100

    Frontend, XLS file reading - Designed new frontend due to loss of code written before - Changed reading excel file library to read XLS file which is original type of file

--- index.js
+++ index.js
@@ -11,133 +11,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-const createLessons = (array, weeks, groups) => {
-    let newWeeks = weeks;
-
-    array.map((lesson, lessonRow) => {
-        if (typeof lesson[0] === "string") {
-            lesson[lesson.length] = lesson.join('/n');
-            newWeeks.map((week, weekIndex) => {
-                Object.keys(week).map((dayName, dayIndex) => {
-                    const day = week[dayName];
-                    if (lessonRow >= day.rowStart && lessonRow <= day.rowEnd) {
-                        Object.keys(day).map((lessonHours) => {
-                            const _lessons = day[lessonHours]
-                            if (typeof _lessons !== "number") {
-                                if (lessonRow >= _lessons.rowStart && lessonRow <= _lessons.rowEnd) {
-                                    newWeeks[weekIndex][dayName][lessonHours].lessons.push({
-                                        name: lesson[lesson.length - 1],
-                                        groups
-                                    })
-                                }
-                            }
-                        })
-                    }
-                })
-            })
-        }
-    })
-
-    return newWeeks;
-}
-
-const createHours = (array) => {
-    let lastKnownObject = {}
-    let hours = [];
-
-    array.map((cellValue, index) => {
-        if (cellValue[0] === "sobota") {
-            if (Object.keys(lastKnownObject).length > 0) {
-                lastKnownObject = {
-                    sobota: lastKnownObject.sobota,
-                    niedziela: {
-                        ...lastKnownObject.niedziela,
-                        rowEnd: index - 2
-                    }
-                }
-                hours.push(lastKnownObject);
-            }
-
-            lastKnownObject = {
-                sobota: {
-                    rowStart: index
-                }
-            }
-        } else if (cellValue[0] === "niedziela") {
-            lastKnownObject = {
-                sobota: {
-                    ...lastKnownObject.sobota,
-                    rowEnd: index - 2
-                },
-                niedziela: {
-                    rowStart: index
-                }
-            }
-        } else {
-            if (cellValue[0]) {
-                Object.keys(lastKnownObject).map((key, _index) => {
-                    if (_index === Object.keys(lastKnownObject).length - 1) {
-                        lastKnownObject[key][cellValue[0]] = {
-                            rowStart: index - 1,
-                            lessons: [],
-                            rowEnd: index + 1
-                        }
-                    }
-                })
-            }
-        }
-    })
-
-    return hours;
-}
-
-const createDates = (array) => {
-    let lastKnownDate = '00.00.0000';
-    let newArray = []
-    array.map((date, index) => {
-        if (typeof date[0] !== "undefined") {
-            if (typeof date[0] === "number") {
-                const _date = XlsxPopulate.numberToDate(date[0]);
-
-                lastKnownDate = `${_date.getDate()}.${_date.getUTCMonth() + 1}.${_date.getFullYear()}`
-            } else {
-                newArray.push(date[0]);
-                lastKnownDate = date[0];
-            }
-        } else {
-            newArray.push(lastKnownDate)
-        }
-    })
-
-    return newArray;
-}
-
-const createArrayFromY1Workbook = (workbook) => {
-
-    const dates = createDates(workbook.sheet('2021_2022').range('A8:A474').value());
-    let array = createHours(workbook.sheet('2021_2022').range('B7:B474').value());
-    dates.map((date, index) => {
-        array.map((week, weekIndex) => {
-            Object.keys(week).map(weekDay => {
-                const dayValue = week[weekDay];
-
-                if (dayValue.rowStart < index && dayValue.rowEnd > index) {
-                    array[weekIndex][weekDay].date = date;
-                }
-            })
-        })
-    })
-
-    array = createLessons(workbook.sheet('2021_2022').range('J8:J474').value(), array, { w: 1, c: 1, l: 1 });
-    array = createLessons(workbook.sheet('2021_2022').range('K8:K474').value(), array, { w: 1, c: 1, l: 2 });
-    array = createLessons(workbook.sheet('2021_2022').range('L8:L474').value(), array, { w: 1, c: 2, l: 3 });
-    array = createLessons(workbook.sheet('2021_2022').range('M8:M474').value(), array, { w: 1, c: 2, l: 4 });
-    array = createLessons(workbook.sheet('2021_2022').range('N8:N474').value(), array, { w: 1, c: 3, l: 5 });
-    array = createLessons(workbook.sheet('2021_2022').range('N8:N474').value(), array, { w: 1, c: 3, l: 6 });
-    array = new Object(array);
-    return array;
-}
-
 const newLessons = []
 
 const data = xlsx.readFile("./plan-2.xls");
@@ -191,15 +64,13 @@ for (let i = startIndex; i <= endIndex; i++) {
         }
 
         newLessons.push({
-            sobota: {
-                date: dayDate,
-                '8:00-10:30': { lessons: [] },
-                '10:45-13:15': { lessons: [] },
-                '14:00-16:30': { lessons: [] },
-                '16:45-19:15': { lessons: [] },
-                rowStart: i,
-                rowEnd: i + 12
-            }
+            date: dayDate,
+            '8:00-10:30': { lessons: [] },
+            '10:45-13:15': { lessons: [] },
+            '14:00-16:30': { lessons: [] },
+            '16:45-19:15': { lessons: [] },
+            rowStart: i,
+            rowEnd: i + 12
         })
     }
 }
@@ -219,8 +90,8 @@ groups.map(group => {
 
         if (field && typeof field.v !== "number") {
             newLessons.map((newLesson, j) => {
-                if (newLesson.sobota.rowStart <= i && newLesson.sobota.rowEnd >= i) {
-                    newLessons[j].sobota[lastHour].lessons.push({
+                if (newLesson.rowStart <= i && newLesson.rowEnd >= i) {
+                    newLessons[j][lastHour].lessons.push({
                         name: field.v,
                         groups: {
                             w: group.w,
@@ -234,25 +105,7 @@ groups.map(group => {
     }
 })
 
-// console.log(newLessons)
-
-// XlsxPopulate.fromFileAsync('./plan-2.xlsx')
-//     .then(workbook => {
-//         let lessons = createArrayFromY1Workbook(workbook)
-
-//         console.log(lessons)
-//     })
-//     .catch(err => console.log(err))
-
 app.get('/lessons', (req, res) => {
-    // XlsxPopulate.fromFileAsync('./plan-2.xlsx')
-    //     .then(workbook => {
-    //         let lessons = createArrayFromY1Workbook(workbook)
-
-    //         res.status(200).json(lessons)
-    //     })
-    //     .catch(err => console.log(err))
-
     res.status(200).json(newLessons)
 })
 
